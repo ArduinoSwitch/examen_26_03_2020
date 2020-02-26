ebb0e41b94c73b9715103037a0c5be587f4a2e52
Andrey <andrey.melnikov@basetis.com>
2020-02-26 20:30:13 +0100

    add postgres fetch data

--- app/src/main/java/com/example/examen/DataBase/PSQLDB/ConnectionPSQL.java
+++ app/src/main/java/com/example/examen/DataBase/PSQLDB/ConnectionPSQL.java
@@ -2,19 +2,48 @@ package com.example.examen.DataBase.PSQLDB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class ConnectionPSQL {
+    ArrayList<EventsModel> listEvents;
     public void inserData() {
-        myThread.start();
-        if (myThread.isAlive()){
+        postData.start();
+        if (postData.isAlive()){
             System.out.println("Hecho!");
         } else {
             System.out.println("Fallo!");
         }
     }
 
-    Thread myThread = new Thread() {
+    Thread getData = new Thread(){
+        Connection conn;
+
+        public void run() {
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection(
+                        "jdbc:postgresql://192.168.0.22:5432/esdeveniments",
+                        "maggy",
+                        "hamilton"
+                );
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM EventDetail;");
+                rs
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    conn.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    Thread postData = new Thread() {
         Connection conn;
 
         public void run() {
---
+++ app/src/main/java/com/example/examen/DataBase/PSQLDB/EventsModel.java
@@ -0,0 +1,12 @@
+package com.example.examen.DataBase.PSQLDB;
+
+public class EventsModel {
+    public String title;
+    public String evdate;
+    public String place;
+    public String email;
+    public String room;
+    public String price;
+    public String people;
+    public String description;
+}
